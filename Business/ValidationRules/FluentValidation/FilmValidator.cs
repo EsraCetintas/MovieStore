a800d420abe08a27cccefecd1c12180511094310
esra.cetintas@outlook.com.tr <esra.cetintas@outlook.com.tr>
2021-08-25 14:41:09 +0300

    Validasyon eklendi

---
+++ Business/ValidationRules/FluentValidation/FilmValidator.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+   public class FilmValidator:AbstractValidator<Film>
+    {
+        public FilmValidator()
+        {
+            RuleFor(p => p.FilmName).NotEmpty();
+            RuleFor(p => p.Price).NotEmpty();
+            RuleFor(p => p.Price).GreaterThan(0);
+        }
+    }
+}
